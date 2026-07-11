ff5004977434eafeac692d7f966f050b804f9b4e
Flin4ek <temskens@gmail.com>
2026-07-11 04:44:33 +0300

    init: первый коммит

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http'); 
+const server = http.createServer((request, response) => {
+  if (request.method === 'GET' && request.url==='/balance'){
+    response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
+    response.end('{"account": "flin4ek", "money": 5000}')
+  }
+  else if(request.method === 'POST' && request.url==='/transfer'){
+    response.writeHead(400, { 'Content-Type': 'text/plain; charset=utf-8' })
+    response.end('Ошибка: нет данных для перевода')
+  }
+  else{
+    response.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' })
+    response.end('Not Found')
+  }
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
