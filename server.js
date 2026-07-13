0db137dbabf1d63f0743f17ed243f1c2edf9a270
Flin4ek <temskens@gmail.com>
2026-07-14 02:11:08 +0300

    добавлено приветствие

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
     response.end('{"account": "flin4ek", "money": 5000}')
   }
+  else if (request.method === 'GET'){
+    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+    response.end('hello bro')
+  }
   else if(request.method === 'POST' && request.url==='/transfer'){
     response.writeHead(400, { 'Content-Type': 'text/plain; charset=utf-8' })
     response.end('Ошибка: нет данных для перевода')
