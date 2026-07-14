06fc010f55f305248fe7a705a9f575aa2a247782
Flin4ek <temskens@gmail.com>
2026-07-15 02:53:02 +0300

    html

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ import 'dotenv/config';
 import http from 'http';
 import path from 'path';
 import url from 'url';
-const server = http.createServer((request, response) => {
+import fs from 'fs/promises';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const htmlPath = path.join(__dirname, 'public', 'index.html');
+console.log("Полный железобетонный путь:", htmlPath);
+const server = http.createServer(async (request, response) => {
   if (request.method === 'GET' && request.url==='/balance'){
     response.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' })
     response.end('{"account": "flin4ek", "money": 5000}')
   }
-  else if (request.method === 'GET' && request.url==='/'){
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-    response.end('hello denchick btw')
+  else if (request.method === 'GET' && request.url === '/') {
+    try {
+      // Ждем, пока fs прочитает файл
+      const htmlContent = await fs.readFile(htmlPath, 'utf-8');
+      
+      // Отдаем HTML в браузер
+      response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+      response.end(htmlContent);
+    } catch (error) {
+      console.error(error);
+      response.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+      response.end('Internal Server Error: не удалось прочитать файл');
+    }
   }
   else if(request.method === 'POST' && request.url==='/transfer'){
     response.writeHead(400, { 'Content-Type': 'text/plain; charset=utf-8' })
@@ -28,7 +43,3 @@ const { PORT = 8080 } = process.env;
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const htmlPath = path.join(__dirname, 'public', 'index.html');
-console.log("Полный железобетонный путь:", htmlPath);
